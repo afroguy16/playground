c895f322cfbf0c2cd63eb1576e04501c9ed250cb
afroguy16 <tega_joe@hotmail.com>
2022-04-13 16:03:30 +0000

    add Typescript && covert Todos Component to TS

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,13 @@ const mode = process.env.NODE_ENV === "production" ? "production" : "development
 module.exports = {
     mode,
 
+    entry: './src/index.ts',
+
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', {loader: 'css-loader'}]
+                use: ['style-loader', { loader: 'css-loader' }]
             },
             {
                 test: /\.scss$/,
@@ -22,14 +24,24 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
-            }
+            },
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ]
     },
-    
+
     devServer: {
         static: {
             directory: path.resolve(__dirname, 'dist'),
-          }
+        }
+    },
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
 
     devtool: "source-map"
